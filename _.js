3b7c13fcf3269e1a8074c852aa3496fa31405a78
Silvanos <ericoderito@gmail.com>
2020-09-16 09:30:02 +0300

    Added inRange method

--- _.js
+++ _.js
@@ -1,13 +1,29 @@
 const _ = {
-  clamp(number, lowerBound, upperBound) {
-    if (number < lowerBound) {
-      return lowerBound;
-    } else if (number > upperBound) {
-      return upperBound;
-    } else {
-      return number;
+    clamp(number, lowerBound, upperBound) {
+        if (number < lowerBound) {
+            return lowerBound;
+        } else if (number > upperBound) {
+            return upperBound;
+        } else {
+            return number;
+        }
+    },
+    inRange(number, startValue, endValue) {
+        if (typeof endValue === 'undefined') {
+            endValue = startValue;
+            startValue = 0;
+        }
+        if (startValue > endValue) {
+            holder = startValue;
+            startValue = endValue;
+            endValue = holder;
+        }
+        if (number >= startValue && number < endValue) {
+            return true;
+        } else if (number < startValue || number >= endValue) {
+            return false;
+        }
     }
-  }
 };
 
 
